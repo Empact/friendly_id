c3a911380de042cc0f6d81ec3935904bf930e12f
Ben Woosley <ben.woosley@gmail.com>
2009-12-07 23:26:24 -0500

    Tiny expected_size cleanup

--- lib/friendly_id/helpers.rb
+++ lib/friendly_id/helpers.rb
@@ -4,7 +4,7 @@ module FriendlyId
     # Calculate expected result size for find_some_with_friendly (taken from
     # active_record/base.rb)
     def expected_size(ids_and_names, options) #:nodoc:#
-      size = options[:offset] ? ids_and_names.size - options[:offset] : ids_and_names.size
+      size =  ids_and_names.size - (options[:offset] || 0)
       size = options[:limit] if options[:limit] && size > options[:limit]
       size
     end
